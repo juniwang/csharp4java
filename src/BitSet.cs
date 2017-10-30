36795e81335b35142d3b1f69751a573d385913fd
Junbo Wang <juniwang@microsoft.com>
2017-10-30 13:20:39 +0800

    Update BitSet.cs

--- src/BitSet.cs
+++ src/BitSet.cs
@@ -1,10 +1,69 @@
+/* BitSet.cs -- A vector of bits.
+    Copyright (C) 1998, 1999, 2000, 2001, 2004, 2005  Free Software Foundation, Inc.
+ 
+ This file is part of GNU Classpath.
+ 
+ GNU Classpath is free software; you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation; either version 2, or (at your option)
+ any later version.
+ 
+ GNU Classpath is distributed in the hope that it will be useful, but
+ WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ General Public License for more details.
+ 
+ You should have received a copy of the GNU General Public License
+ along with GNU Classpath; see the file COPYING.  If not, write to the
+ Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+ 02110-1301 USA.
+ 
+ Linking this library statically or dynamically with other modules is
+ making a combined work based on this library.  Thus, the terms and
+ conditions of the GNU General Public License cover the whole
+ combination.
+ 
+ As a special exception, the copyright holders of this library give you
+ permission to link this library with independent modules to produce an
+ executable, regardless of the license terms of these independent
+ modules, and to copy and distribute the resulting executable under
+ terms of your choice, provided that you also meet, for each linked
+ independent module, the terms and conditions of the license of that
+ module.  An independent module is a module which is not derived from
+ or based on this library.  If you modify this library, you may extend
+ this exception to your version of the library, but you are not
+ obligated to do so.  If you do not wish to do so, delete this
+ exception statement from your version. */
+
 using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace Util
 {
-
+    /// <summary>
+    /// This class can be thought of in two ways.  You can see it as a
+    /// vector of bits or as a set of non-negative integers.  The name
+    /// <code>BitSet</code> is a bit misleading.
+    /// 
+    /// It is implemented by a bit vector, but its equally possible to see
+    /// it as set of non-negative integer; each integer in the set is
+    /// represented by a set bit at the corresponding index.  The size of
+    /// this structure is determined by the highest integer in the set.
+    /// 
+    /// You can union, intersect and build (symmetric) remainders, by
+    /// invoking the logical operations and, or, andNot, resp. xor.
+    /// 
+    /// This implementation is NOT synchronized against concurrent access from
+    /// multiple threads. Specifically, if one thread is reading from a bitset
+    /// while another thread is simultaneously modifying it, the results are
+    /// undefined.
+    /// 
+    /// author Jochen Hoenicke
+    /// author Tom Tromey (tromey@cygnus.com)
+    /// author Eric Blake (ebb9@email.byu.edu)
+    /// status updated to 1.4
+    /// </summary>
     [Serializable]
     public class BitSet
     {
